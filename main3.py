a6d0a1378a6a9a42efa57909f6d1e29598eb66d6
Percedal15019 <vedant3066@gmail.com>
2025-09-16 20:20:15 +0530

    Update main3.py

--- main3.py
+++ main3.py
@@ -439,9 +439,10 @@ st.markdown("""
 # 4. Run the app: `streamlit run app.py`
 
 # streamlit==1.28.0
-# streamlit-drawable-canvas==0.9.3
+streamlit-drawable-canvas==0.9.3
 # Pillow==10.0.1
 # pandas==2.1.1
 
 # sqlite3
+
 
